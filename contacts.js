72d13c06d0c6d4a3fe9956d10d1d5f94251e9911
OleksandrKambarov <113207904+OleksandrKambarov@users.noreply.github.com>
2023-06-07 21:32:11 +0300

    create-function-listContacts

---
+++ contacts.js
@@ -0,0 +1,10 @@
+const fs = require("fs").promises;
+const path = require("node:path");
+
+const contactsPath = path.resolve("db", "contacts.json");
+
+function listContacts() {
+  fs.readFile(contactsPath)
+    .then((data) => console.table(JSON.parse(data)))
+    .catch((error) => console.log(error.message));
+}
