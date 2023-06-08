482421869d2679309914610617dc045b1dd3c14b
OleksandrKambarov <113207904+OleksandrKambarov@users.noreply.github.com>
2023-06-08 17:41:24 +0300

    add-contactId

--- contacts.js
+++ contacts.js
@@ -9,10 +9,21 @@ const listContacts = async () => {
   return JSON.parse(data);
 };
 
-const getContactById = async (Id) => {
+const getContactById = async (contactId) => {
   const contacts = await listContacts();
-  const result = contacts.find((item) => item.Id === id);
+  const result = contacts.find((item) => item.Id === contactId);
   return result || null;
+};
+
+const removeContact = async (contactId) => {
+  const contacts = await listContacts();
+  const index = contacts.findIndex((item) => item.id === contactId);
+  if (index === -1) {
+    return null;
+  }
+  const [result] = contacts.splice(index, 1);
+  await fs.writeFile(contactsPath, JSON.stringify(contacts, null, 2));
+  return result;
 };
 
 const addContact = async (data) => {
@@ -24,17 +35,6 @@ const addContact = async (data) => {
   contacts.push(newContact);
   await fs.writeFile(contactsPath, JSON.stringify(contacts, null, 2));
   return newContact;
-};
-
-const removeContact = async (id) => {
-  const contacts = await listContacts();
-  const index = contacts.findIndex((item) => item.id === id);
-  if (index === -1) {
-    return null;
-  }
-  const [result] = contacts.splice(index, 1);
-  await fs.writeFile(contactsPath, JSON.stringify(contacts, null, 2));
-  return result;
 };
 
 module.exports = { listContacts, getContactById, addContact, removeContact };
